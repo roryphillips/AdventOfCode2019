315854f1df8b0887227c15d5028648880cc354e0
Rory Phillips <roryphillipsdev@gmail.com>
2019-12-02 19:55:22 +1000

    [master]: Added intcode tests

---
+++ intcode.test.js
@@ -0,0 +1,63 @@
+const { tableTest } = require('./utils');
+const { evaluateIntCode } = require('./intcode');
+
+describe('evaluateIntCode', () => {
+  const conditions = [
+    {
+      input: [
+        1, 0, 0, 0,
+        99
+      ],
+      expected: [
+        2, 0, 0, 0,
+        99
+      ]
+    },
+    {
+      input: [
+        2, 3, 0, 3,
+        99
+      ],
+      expected: [
+        2, 3, 0, 6,
+        99
+      ]
+    },
+    {
+      input: [
+        2, 4, 4, 5,
+        99, 0
+      ],
+      expected: [
+        2, 4, 4, 5,
+        99, 9801
+      ]
+    },
+    {
+      input: [
+        1, 1, 1, 4,
+        99, 5, 6, 0,
+        99
+      ],
+      expected: [
+        30, 1, 1, 4,
+        2, 5, 6, 0,
+        99
+      ]
+    },
+    {
+      input: [
+        1, 9, 10, 3,
+        2, 3, 11, 0,
+        99, 30, 40, 50
+      ],
+      expected: [
+        3500, 9, 10, 70,
+        2, 3, 11, 0,
+        99, 30, 40, 50
+      ]
+    }
+  ];
+
+  tableTest(conditions, evaluateIntCode, 'toStrictEqual');
+});
